ac504e2cfd15a7d2c4715436768e5cfd81260f6f
Francois <francois.onimus@gmail.com>
2017-04-15 19:33:06 +0200

    Update .travis.yml

--- test/generate.sh
+++ test/generate.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-echo 'Generating project with following configuration : $1'
+echo '>> Generating project with following configuration : $1'
 mkdir $HOME/angutest
 cd $HOME/angutest
-cp $HOME/test/.yo-rc.$1.json $HOME/angutest/
+cp $TRAVIS_BUILD_DIR/test/.yo-rc.$1.json $HOME/angutest/
 yo anguboot2 -f
+echo '>> Generation done !'
